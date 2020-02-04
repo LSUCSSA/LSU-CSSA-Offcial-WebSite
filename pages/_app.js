d889703cc9e33ca8d135d22f4dd2f8ac725483af
wate123 <junlin0604@gmail.com>
2020-02-04 00:50:19 -0600

    small adjustment

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,27 @@
 import './styles.css';
-import { Row, Col } from 'antd';
+import { Row, Col, Typography, Affix } from 'antd';
 import Link from 'next/link'
 import { withRouter } from 'next/router';
+
+const { Title } = Typography;
+
 
 function MyApp({ router, Component, pageProps }) {
     return (
         <div className="App">
             {
-                router.pathname === '/' ? <div type="flex" className="Header">
-                    <img src={require('../public/images/8PXUyg.jpg')} alt="banner" />
+                router.pathname === '/' ? <div className="ImageHeader">
+                    <Affix>
+                        <Row type="flex" align="middle" justify="end" style={{ paddingTop: "5%", color: "white"}}>
+                            <Col span={3}><Link href="/"><span className="nav">主页</span></Link></Col>
+                            <Col span={3}><Link href="/events"><span className="nav">活动</span></Link></Col>
+                            <Col span={3}><Link href="/"><span className="nav">校园资讯</span></Link></Col>
+                            <Col span={3}><Link href="/about"><span className="nav">关于我们</span></Link></Col>
+                        </Row>
+                    </Affix>
                 </div> : null
             }
-            <Row type="flex" align="middle" justify="space-around" style={{ margin: "20px" }}>
-                <Col span={6}><Link href="/"><span className="nav">主页</span></Link></Col>
-                <Col span={6}><Link href="/events"><span className="nav">活动</span></Link></Col>
-                <Col span={6}><Link href="/"><span className="nav">校园资讯</span></Link></Col>
-                <Col span={6}><Link href="/about"><span className="nav">关于我们</span></Link></Col>
-            </Row>
+            
             <Component style="Container" {...pageProps} />
             <div>
                 <Row className="Footer" type="flex" align="middle" justify="center">
